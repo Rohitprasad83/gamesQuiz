56de4b54d7e902e6e4e22103400de32a8d5a7dbb
Rohit Prasad <pdrohit674@gmail.com>
2022-09-30 21:47:25 +0530

    feat: added react-router and components setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-
+import {Home,LandingPage, Login, Signup,Error404} from './features/index'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/features/index.tsx
@@ -0,0 +1,7 @@
+import { Home } from './home/Home'
+import {LandingPage} from './home/LandingPage'
+import { Login } from './auth/Login'
+import {Signup} from './auth/Signup'
+import { Error404 } from './notFound404/Error404'
+
+export {Home, LandingPage, Login,Signup,Error404}
